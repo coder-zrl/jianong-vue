5c3eda94021f302fb6b3c21ac5edbb6e17831ee4
liyankun.lyk <liyankun.lyk@alibaba-inc.com>
2022-04-13 13:53:44 +0800

    两个图

---
+++ src/views/modules/renren/purchase-situation.vue
@@ -0,0 +1,332 @@
+<template>
+  <div class="mod-demo-echarts">
+    <el-alert
+      title="提示："
+      type="warning"
+      :closable="false">
+      <div slot-scope="description">
+        <p class="el-alert__description">1. 此Demo只提供ECharts官方使用文档，入门部署和体验功能。具体使用请参考：http://echarts.baidu.com/index.html</p>
+      </div>
+    </el-alert>
+
+    <el-row>
+      <el-col :span="24">
+        <el-card>
+          <div id="J_chartBarBox" class="chart-box"></div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card>
+          <div id="J_chartPieBox" class="chart-box"></div>
+        </el-card>
+      </el-col>
+      <el-col :span="12">
+        <el-card>
+          <div id="J_chartScatterBox" class="chart-box"></div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+  import echarts from 'echarts'
+  export default {
+    data () {
+      return {
+        chartLine: null,
+        chartBar: null,
+        chartPie: null,
+        chartScatter: null
+      }
+    },
+    mounted () {
+      // this.initChartLine()
+      this.initChartBar()
+      this.initChartPie()
+      this.initChartScatter()
+    },
+    activated () {
+      // 由于给echart添加了resize事件, 在组件激活时需要重新resize绘画一次, 否则出现空白bug
+      if (this.chartLine) {
+        this.chartLine.resize()
+      }
+      if (this.chartBar) {
+        this.chartBar.resize()
+      }
+      if (this.chartPie) {
+        this.chartPie.resize()
+      }
+      if (this.chartScatter) {
+        this.chartScatter.resize()
+      }
+    },
+    methods: {
+      // 折线图
+      initChartLine () {
+        var option = {
+          'title': {
+            'text': '折线图堆叠'
+          },
+          'tooltip': {
+            'trigger': 'axis'
+          },
+          'legend': {
+            'data': [ '出库量', '入库量' ]
+          },
+          'grid': {
+            'left': '3%',
+            'right': '4%',
+            'bottom': '3%',
+            'containLabel': true
+          },
+          'toolbox': {
+            'feature': {
+              'saveAsImage': { }
+            }
+          },
+          'xAxis': {
+            'type': 'category',
+            'boundaryGap': false,
+            'data': [ '周一', '周二', '周三', '周四', '周五', '周六', '周日' ]
+          },
+          'yAxis': {
+            'type': 'value'
+          },
+          'series': [
+            {
+              'name': '出库量',
+              'type': 'line',
+              'stack': '总量',
+              'data': [ 120, 132, 101, 134, 90, 230, 210 ]
+            },
+            {
+              'name': '入库量',
+              'type': 'line',
+              'stack': '总量',
+              'data': [ 220, 182, 191, 234, 290, 330, 310 ]
+            }
+          ]
+        }
+        this.chartLine = echarts.init(document.getElementById('J_chartLineBox'))
+        this.chartLine.setOption(option)
+        window.addEventListener('resize', () => {
+          this.chartLine.resize()
+        })
+      },
+      // 柱状图
+      initChartBar () {
+        var option = {
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+              type: 'shadow'
+            }
+          },
+          legend: {
+            data: ['购买量']
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              data: ['产品1', '产品2', '产品3', '产品4', '产品5', '产品6', '产品7']
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+              name: '购买量',
+              type: 'bar',
+              data: [120, 132, 101, 134, 90, 230, 210],
+              itemStyle: {
+                borderWidth: 0.5,
+                color: 'rgba(28, 151, 196, 1)'
+              },
+              barWidth: '40%'
+            }
+          ]
+        }
+        this.chartBar = echarts.init(document.getElementById('J_chartBarBox'))
+        this.chartBar.setOption(option)
+        window.addEventListener('resize', () => {
+          this.chartBar.resize()
+        })
+      },
+      // 饼状图
+      initChartPie () {
+        var option = {
+          backgroundColor: '#2c343c',
+          title: {
+            text: 'Customized Pie',
+            left: 'center',
+            top: 20,
+            textStyle: {
+              color: '#ccc'
+            }
+          },
+          tooltip: {
+            trigger: 'item',
+            formatter: '{a} <br/>{b} : {c} ({d}%)'
+          },
+          visualMap: {
+            show: false,
+            min: 80,
+            max: 600,
+            inRange: {
+              colorLightness: [0, 1]
+            }
+          },
+          series: [
+            {
+              name: '访问来源',
+              type: 'pie',
+              radius: '55%',
+              center: ['50%', '50%'],
+              data: [
+                { value: 335, name: '产品1' },
+                { value: 310, name: '产品2' },
+                { value: 274, name: '产品3' },
+                { value: 235, name: '产品4' },
+                { value: 400, name: '产品5' }
+              ].sort(function (a, b) { return a.value - b.value }),
+              roseType: 'radius',
+              label: {
+                normal: {
+                  textStyle: {
+                    color: 'rgba(255, 255, 255, 0.7)'
+                  }
+                }
+              },
+              labelLine: {
+                normal: {
+                  lineStyle: {
+                    color: 'rgba(255, 255, 255, 0.3)'
+                  },
+                  smooth: 0.2,
+                  length: 10,
+                  length2: 20
+                }
+              },
+              itemStyle: {
+                normal: {
+                  color: '#cffaaa',
+                  shadowBlur: 200,
+                  shadowColor: 'rgba(0, 0, 0, 0.5)'
+                }
+              },
+              animationType: 'scale',
+              animationEasing: 'elasticOut',
+              animationDelay: function (idx) {
+                return Math.random() * 200
+              }
+            }
+          ]
+        }
+        this.chartPie = echarts.init(document.getElementById('J_chartPieBox'))
+        this.chartPie.setOption(option)
+        window.addEventListener('resize', () => {
+          this.chartPie.resize()
+        })
+      },
+      // 散点图
+      initChartScatter () {
+        var option = {
+          title: {
+            text: 'Stacked Area Chart'
+          },
+          tooltip: {
+            trigger: 'axis',
+            axisPointer: {
+            type: 'cross',
+            label: {
+                backgroundColor: '#6a7985'
+            }
+            }
+          },
+          legend: {
+            data: ['男性', '女性']
+          },
+          toolbox: {
+            feature: {
+              saveAsImage: {}
+            }
+          },
+          grid: {
+            left: '3%',
+            right: '4%',
+            bottom: '3%',
+            containLabel: true
+          },
+          xAxis: [
+            {
+              type: 'category',
+              boundaryGap: false,
+              data: ['25岁', '30岁', '35岁', '40岁', '45岁', '50岁', '55岁']
+            }
+          ],
+          yAxis: [
+            {
+              type: 'value'
+            }
+          ],
+          series: [
+            {
+                name: '女性',
+                type: 'line',
+                stack: 'Total',
+                areaStyle: {},
+                emphasis: {
+                    focus: 'series'
+                },
+                data: [120, 132, 101, 134, 90, 230, 210]
+            },
+            {
+                name: '男性',
+                type: 'line',
+                stack: 'Total',
+                areaStyle: {},
+                emphasis: {
+                    focus: 'series'
+                },
+                data: [220, 182, 191, 234, 290, 330, 310]
+            }
+          ]
+        }
+        this.chartPie = echarts.init(document.getElementById('J_chartScatterBox'))
+        this.chartPie.setOption(option)
+        window.addEventListener('resize', () => {
+          this.chartPie.resize()
+        })
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .mod-demo-echarts {
+    > .el-alert {
+      margin-bottom: 10px;
+    }
+    > .el-row {
+      margin-top: -10px;
+      margin-bottom: -10px;
+      .el-col {
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    .chart-box {
+      min-height: 400px;
+    }
+  }
+</style>
