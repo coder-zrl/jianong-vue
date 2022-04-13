a602f66cc8115f9e18d0e2f124b6a8f26a799c42
coder-zrl <970586718@qq.com>
2022-04-13 13:12:30 +0800

    fix:remote address

--- static/config/index-prod.js
+++ static/config/index-prod.js
@@ -5,7 +5,7 @@
   window.SITE_CONFIG = {};
 
   // api接口请求地址
-  window.SITE_CONFIG['baseUrl'] = 'http://demo.open.renren.io/renren-fast-server';
+  window.SITE_CONFIG['baseUrl'] = 'http://47.103.198.84:8080/jianong';
 
   // cdn地址 = 域名 + 版本号
   window.SITE_CONFIG['domain']  = './'; // 域名
